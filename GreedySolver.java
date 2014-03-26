f32b128e57e76f8ba2cdd8d6d1dc5e0cc87bd1c6
Anish <hoopanish@gmail.com>
2014-03-26 13:48:55 +0530

    greedySolver

---
+++ GreedySolver.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class GreedySolver extends GameSolver {
+
+    @Override
+    public Edge getNextMove(final Board board, int color) {
+        
+        ArrayList<Edge> moves = board.getAvailableMoves();
+        int moveCount
+        int value[moveCount];
+        
+        Board tempBoard = new Board;
+        tempBoard = board;
+
+        for(int i=0;i<moveCount;i++){
+        	value[i] = heuristic(tempboard.getNewBoard(moves[i],color),color);
+        }
+
+        int maxValueIndex=0;
+        for(int i=0;i<moveCount;i++){
+        	if(value[i]>value[maxValueIndex])
+        		maxValueIndex=i;
+        }
+
+        return moves[maxValueIndex];
+       
+    }
+
+}
